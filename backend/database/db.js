123e30d5ae9ae6e6c5fa5cdef7f707160588ccc0
leaula21 <Lola.mounier.lm@gmail.com>
2024-11-25 17:52:35 +0100

    loging to database successfull

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,9 +1,12 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
-dotenv.config();  // Charge les variables d'environnement
 
+// Spécifie le chemin vers le fichier .env à la racine du projet
+dotenv.config({ path: '../.env' });  // Remonte d'un niveau depuis /back
 
-// Crée une connexion à la base de données avec mysql2
+console.log('DB_USER:', process.env.DB_USER);  // Vérifie que la variable est bien lue
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD); 
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -11,5 +14,13 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
 });
 
-// Exportation de l'objet pool avec une promesse
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Erreur de connexion à la base de données:', err);
+    return;
+  }
+  console.log('Connexion à la base de données réussie');
+  connection.release();
+});
+
 export default pool.promise();
